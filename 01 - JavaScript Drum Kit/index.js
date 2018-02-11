15f30478dc103dae599579d9bdab2f54fedce77e
Gala <galagalunia@gmail.com>
2018-02-11 13:28:04 -0500

    Implement js functionality

---
+++ 01 - JavaScript Drum Kit/index.js
@@ -0,0 +1,39 @@
+function playSound(e) {
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+	if (!audio) return; //stop the function from running
+	
+	audio.currentTime = 0; //will restart the sound every time the button is down
+	audio.play();
+	key.classList.add('playing');
+}
+
+function removeTransition(e) {
+	if (e.propertyName !== 'transform') return;
+	this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend',removeTransition));
+
+window.addEventListener('keydown', playSound);
+
+
+// window.addEventListener('keydown', function(e) {
+// 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+// 	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+// 	if (!audio) return; //stop the function from running
+// 	audio.currentTime = 0; //will restart the sound every time the button is down
+// 	audio.play();
+// 	key.classList.add('playing');
+// });
+
+// function removeTransition(e) {
+// 	console.log(e);
+// 	if (e.propertyName !== 'transform') return;
+	
+// 	this.classList.remove('playing');
+// }
+
+// const keys = document.querySelectorAll('.key');
+// keys.forEach(key => key.addEventListener('transitionend',removeTransition));
